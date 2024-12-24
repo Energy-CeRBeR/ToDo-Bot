696799a262a0580e857f5991698c6672f6b81ce4
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-12-24 11:31:12 +0300

    fix refresh access token logic

--- src/users/handlers.py
+++ src/users/handlers.py
@@ -18,28 +18,6 @@ async def start_bot(message: Message):
     await message.answer(USER_LEXICON_COMMANDS[message.text], parse_mode="Markdown")
 
 
-@router.message(Command(commands="cerber"))
-async def cerber_auth(message: Message, state: FSMContext):
-    tokens = await user_service.login_user("cerber17", "energy_cerber")
-    if tokens:
-        await state.update_data(
-            access_token=tokens["access_token"],
-            refresh_token=tokens["refresh_token"]
-        )
-
-        user_data = await state.get_data()
-        user_dict = await user_service.get_current_user(user_data.setdefault("access_token", "default"))
-        await message.answer(
-            text=USER_LEXICON_COMMANDS["/profile"].format(
-                name=user_dict["name"], surname=user_dict["surname"], email=user_dict["email"]
-            ), parse_mode="Markdown",
-            reply_markup=profile_keyboard()
-        )
-
-    else:
-        await message.answer(text=USER_LEXICON["invalid_auth"])
-
-
 @router.message(Command(commands="help"), StateFilter(default_state))
 async def help_info(message: Message):
     await message.answer(text=USER_LEXICON_COMMANDS[message.text], parse_mode="Markdown")
@@ -49,8 +27,13 @@ async def help_info(message: Message):
 async def start_auth(message: Message, state: FSMContext):
     user_data = await state.get_data()
     user_dict = await user_service.get_current_user(user_data.setdefault("access_token", "default"))
+    if user_dict is None:
+        refresh_data = await user_service.refresh_access(user_data.setdefault("refresh_token", "default"))
+        if refresh_data is not None:
+            await state.update_data(access_token=refresh_data["access_token"])
+            user_dict = await user_service.get_current_user(refresh_data["access_token"])
 
-    if user_dict:
+    if user_dict is not None:
         await message.answer(text=USER_LEXICON_COMMANDS[message.text]["already_authorized"])
     else:
         await message.answer(text=USER_LEXICON_COMMANDS[message.text]["not_authorized"])
@@ -98,8 +81,13 @@ async def get_password(message: Message, state: FSMContext):
 async def show_profile(message: Message, state: FSMContext):
     user_data = await state.get_data()
     user_dict = await user_service.get_current_user(user_data.setdefault("access_token", "default"))
+    if user_dict is None:
+        refresh_data = await user_service.refresh_access(user_data.setdefault("refresh_token", "default"))
+        if refresh_data is not None:
+            await state.update_data(access_token=refresh_data["access_token"])
+            user_dict = await user_service.get_current_user(refresh_data["access_token"])
 
-    if user_dict:
+    if user_dict is not None:
         await message.answer(
             text=USER_LEXICON_COMMANDS[message.text].format(
                 name=user_dict["name"], surname=user_dict["surname"], email=user_dict["email"]
@@ -114,8 +102,13 @@ async def show_profile(message: Message, state: FSMContext):
 async def start_logout(message: Message, state: FSMContext):
     user_data = await state.get_data()
     user_dict = await user_service.get_current_user(user_data.setdefault("access_token", "default"))
+    if user_dict is None:
+        refresh_data = await user_service.refresh_access(user_data.setdefault("refresh_token", "default"))
+        if refresh_data is not None:
+            await state.update_data(access_token=refresh_data["access_token"])
+            user_dict = await user_service.get_current_user(refresh_data["access_token"])
 
-    if user_dict:
+    if user_dict is not None:
         await message.answer(
             text=USER_LEXICON_COMMANDS[message.text]["confirm"],
             reply_markup=yes_no_keyboard("logout")
--- utils/middleware.py
+++ utils/middleware.py
@@ -20,13 +20,10 @@ class AuthMiddleware(BaseMiddleware):
         user_data = await state.get_data()
         user_dict = await user_service.get_current_user(user_data.setdefault("access_token", "default"))
 
-        if not user_dict:
-            refresh_tokens = await user_service.refresh_access(user_data.setdefault("refresh_token", "default"))
-            if refresh_tokens is not None:
-                await state.update_data(
-                    access_token=refresh_tokens["access_token"],
-                    refresh_token=refresh_tokens["refresh_token"]
-                )
+        if user_dict is None:
+            refresh_data = await user_service.refresh_access(user_data.setdefault("refresh_token", "default"))
+            if refresh_data is not None:
+                await state.update_data(access_token=refresh_data["access_token"])
             else:
                 await event.answer(text=USER_LEXICON["not_auth"])
                 return 0
